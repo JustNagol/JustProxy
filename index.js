482c51505b2da349421e6e3365611de16a40a4e4
grant <grantschmidt8@gmail.com>
2026-03-01 16:47:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use(express.static(join(__dirname, "public")));
+
+app.get("/g", (_req, res) => res.sendFile(join(__dirname, "public/games.html")));
+app.get("/m", (_req, res) => res.sendFile(join(__dirname, "public/media.html")));
+app.get("/s", (_req, res) => res.sendFile(join(__dirname, "public/settings.html")));
+
 app.use("*", (_req, res) => {
   res.sendFile(join(__dirname, "public/index.html"));
 });
