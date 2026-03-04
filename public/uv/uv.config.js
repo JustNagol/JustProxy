eb5b883f52a9c6a13515bed75d426012c3afa1c7
grant <grantschmidt8@gmail.com>
2026-03-03 19:25:13 -0600

    scream 7 sucks

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,8 +2,8 @@
 self.__uv$config = {
   prefix: "/uv/service/",
   bare: "https://bare-server-production-a258.up.railway.app/",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
+  encodeUrl: Ultraviolet.codec.plain.encode,
+  decodeUrl: Ultraviolet.codec.plain.decode,
   handler: "/uv/uv.handler.js",
   bundle: "/uv/uv.bundle.js",
   config: "/uv/uv.config.js",
